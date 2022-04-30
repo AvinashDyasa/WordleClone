361d3fad430f8914486fbfdcc0877aa55871f3b6
AvinashDyasa <81165920+AvinashDyasa@users.noreply.github.com>
2022-04-30 14:32:21 +0530

    Add files via upload

--- wordle.py
+++ wordle.py
@@ -3,14 +3,21 @@ from itertools import count
 import random
 words = []
 count = 0
-with open(sgb-words.txt) as f:
+with open('sgb-words.txt') as f:
     for line in f:
         words.append(line.strip('\n'))
 random_index = random.randint(0, len(words)-1)
 ans = words[random_index]
+
+
 def ask():
     global guess
-    guess = input('Guess:')
+    guess = input('''   "*" will represnt correct letter and correct position of the alphabet 
+   "-" will represnt correct letter but wrong position of the alphabet
+   "_" will represent wrong letter and wrong position of the alphabet 
+         Guess:''')
+
+
 def comp(guess, ans):
     result = []
     global count
@@ -23,25 +30,28 @@ def comp(guess, ans):
         else:
             result.append("_")
     print(result)
-    if count<6:
+    if count < 6:
         ask()
         check()
     else:
         print("Better luck next time")
+
+
 def check():
-    if(len(guess) == 5 ):
-        
-            if (bool(guess in words) == False):
-                print("word is not in dictionary")
-            elif guess==ans:
-                print("Correct guess")
-            else:
-                comp(guess, ans)
-            
+    if(len(guess) == 5):
+
+        if (bool(guess in words) == False):
+            print("word is not in dictionary")
+        elif guess == ans:
+            print("Correct guess")
+        else:
+            comp(guess, ans)
+
     elif(count == 6):
         print("Better luck next time")
     else:
         print('Word should be 5 letters long')
+
 
 ask()
 check()
